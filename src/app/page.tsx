e442138430e81bccec3d41f57b0b230d5ffcb638
KPR23 <zabluka_aehit@students.vizja.pl>
2025-02-11 17:57:50 +0100

    CI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import Layout from "~/components/layout"
-import EmployeeList from "~/components/employee-list"
-import { employees } from "~/lib/mockData"
+import Layout from "~/components/layout";
+import EmployeeList from "~/components/employee-list";
+import { employees } from "~/lib/mockData";
 
 export default function Home() {
   return (
     <Layout>
       <div className="px-4 py-6 sm:px-0">
-        <h1 className="text-2xl font-semibold text-gray-900 mb-6">Employee List</h1>
+        <h1 className="mb-6 text-2xl font-semibold text-gray-900">
+          Employee List
+        </h1>
         <EmployeeList employees={employees} />
+        <div className="mt-6">hs</div>
       </div>
     </Layout>
-  )
+  );
 }
-
