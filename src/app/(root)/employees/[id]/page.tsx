f3b9ad586aba4aa54cf4e05c59977ad635ecb7ad
KPR23 <zabluka_aehit@students.vizja.pl>
2025-02-26 20:50:05 +0100

    fixing vercel errors

--- src/app/(root)/employees/[id]/page.tsx
+++ src/app/(root)/employees/[id]/page.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { EmployeeService } from "~/services/employeeService";
 
-const page = async ({ params }: { params: { id: string } }) => {
-  const employee = await EmployeeService.getById(Number(params.id));
+interface Props {
+  params: {
+    id: string;
+  };
+}
 
-  return (
-    <div>
-      <h1>
-        {employee?.firstName} {employee?.lastName}
-      </h1>
-    </div>
-  );
+const Page = async ({ params }: Props) => {
+  try {
+    const employee = await EmployeeService.getById(Number(params.id));
+
+    return (
+      <div>
+        <h1>
+          {employee?.firstName} {employee?.lastName}
+        </h1>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching employee:", error);
+    throw new Error("Nie udało się załadować listy pracowników.");
+  }
 };
 
-export default page;
+export default Page;
